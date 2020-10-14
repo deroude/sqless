3db97061a1432141726199d98d67352110a6d90f
Valentin Raduti <valentin@thedotin.ro>
2020-10-14 19:23:03 +0300

    initial sql templates

--- core/src/business/Generator.ts
+++ core/src/business/Generator.ts
@@ -9,6 +9,36 @@ global.Handlebars = Handlebars;
 
 import './templates/precompiled';
 
+type PGType = 'serial' | 'varchar' | 'decimal' | 'int' | 'bigint' | 'boolean' | 'timestamptz';
+
+interface Property {
+    name: string;
+    type: PGType,
+    isId: boolean;
+    isRequired: boolean;
+}
+
+interface Entity {
+    name: string;
+    properties: Property[];
+}
+
+const typeMap: { [k: string]: PGType } = {
+    'string': 'varchar',
+    'string:date': 'timestamptz',
+    'string:date-time': 'timestamptz',
+    'string:password': 'varchar',
+    'integer': 'int',
+    'integer:int32': 'int',
+    'integer:int64': 'bigint',
+    'number': 'decimal',
+    'number:float': 'decimal',
+    'number:double': 'decimal',
+    'boolean': 'boolean'
+}
+
+const toSnake = (a: string) => a.split(/(?=[A-Z])/).join('_').toLowerCase();
+
 export class Generator {
     constructor(private config: GeneratorConfig) { }
 
@@ -34,7 +64,30 @@ export class Generator {
             return Promise.reject('No API file available');
         }
 
+        const entities = [];
+
+        for (const [k, v] of Object.entries(api.components.schemas)) {
+            const entity: Entity = { name: toSnake(k), properties: [] };
+            const schema: OpenAPIV3.SchemaObject = v as OpenAPIV3.SchemaObject;
+            for (const [pk, pv] of Object.entries(schema.properties)) {
+                // TODO treat references
+                const ps: OpenAPIV3.SchemaObject = pv as OpenAPIV3.SchemaObject;
+                const property: Property = {
+                    name: toSnake(pk),
+                    isId: pk.toLowerCase() === 'id',
+                    isRequired: schema.required && schema.required.indexOf(pk.toLowerCase()) > 0,
+                    type: typeMap[ps.format ? `${ps.type}:${ps.format}` : ps.type]
+                }
+                entity.properties.push(property);
+            }
+            entities.push(entity);
+        }
+
         await this.writeFile('.sqless/sqless-config.yaml', Handlebars.templates['sqless-config.yaml'], { apiPath: this.config.apiPath.replace(/^\.[\/\\]/, '') });
+        await this.writeFile('.sqless/docker-compose.yaml', Handlebars.templates['docker-compose.yaml'], {});
+        await this.writeFile('.sqless/postgres-init.sql', Handlebars.templates['postgres-init.sql'], {});
+        await this.writeFile('.sqless/migrations/001_initial.sql', Handlebars.templates['001_initial.sql'], { entities });
+
 
         return Promise.resolve();
     }
