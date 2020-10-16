dfb0260b21cd159fd0d3bd5a9697ca64dbc77401
Valentin Raduti <valentin@thedotin.ro>
2020-10-16 18:05:28 +0300

    added generators for queries

--- core/src/business/Generator.ts
+++ core/src/business/Generator.ts
@@ -13,15 +13,25 @@ type PGType = 'serial' | 'varchar' | 'decimal' | 'int' | 'bigint' | 'boolean' | 
 
 interface Property {
     name: string;
+    nameSnake: string;
     type: PGType,
     isId: boolean;
     isRequired: boolean;
     fk?: string;
+    index: number;
+}
+
+interface Operation {
+    method: string;
+    delegate: string;
 }
 
 interface Entity {
     name: string;
+    nameSnake: string;
     properties: Property[];
+    operations: Operation[];
+    lastIndex: number;
 }
 
 const typeMap: { [k: string]: PGType } = {
@@ -45,6 +55,7 @@ function isReference(prop: OpenAPIV3.SchemaObject | OpenAPIV3.ReferenceObject): 
 }
 
 const refPattern = /^#\/components\/schemas\/(.+)$/;
+const pathPattern = /^\/([^\/]+)$/;
 
 export class Generator {
     constructor(private config: GeneratorConfig) { }
@@ -74,14 +85,17 @@ export class Generator {
         const entities = [];
 
         for (const [k, v] of Object.entries(api.components.schemas)) {
-            const entity: Entity = { name: toSnake(k), properties: [] };
+            const entity: Entity = { name: k, nameSnake: toSnake(k), properties: [], operations: [], lastIndex: 0 };
             const schema: OpenAPIV3.SchemaObject = v as OpenAPIV3.SchemaObject;
+            let index = 1;
             for (const [pk, pv] of Object.entries(schema.properties)) {
                 if (isReference(pv)) {
                     const ref = pv.$ref.match(refPattern);
                     if (ref && ref[1]) {
                         entity.properties.push({
-                            name: toSnake(pk),
+                            name: pk,
+                            nameSnake: toSnake(pk),
+                            index,
                             isId: false,
                             isRequired: schema.required && schema.required.indexOf(pk.toLowerCase()) > 0,
                             type: 'int',
@@ -90,17 +104,21 @@ export class Generator {
                     }
                 } else {
                     entity.properties.push({
-                        name: toSnake(pk),
+                        name: pk,
+                        nameSnake: toSnake(pk),
                         isId: pk.toLowerCase() === 'id',
                         isRequired: schema.required && schema.required.indexOf(pk.toLowerCase()) > 0,
+                        index,
                         type: typeMap[pv.format ? `${pv.type}:${pv.format}` : pv.type]
                     })
                 }
+                index++;
             }
 
-            entities.push(entity);
+            entities.push({ ...entity, lastIndex: index });
         }
 
+        // Order entities according to the references
         let iter = 0;
         let sw = true;
         while (sw && iter++ < 10) {
@@ -122,15 +140,47 @@ export class Generator {
             }
         }
 
-        if(iter === 10){
+        if (iter === 10) {
             console.warn('Circular FK detected');
         }
+
+
 
         await this.writeFile('.sqless/sqless-config.yaml', Handlebars.templates['sqless-config.yaml'], { apiPath: this.config.apiPath.replace(/^\.[\/\\]/, '') });
         await this.writeFile('.sqless/docker-compose.yaml', Handlebars.templates['docker-compose.yaml'], {});
         await this.writeFile('.sqless/postgres-init.sql', Handlebars.templates['postgres-init.sql'], {});
         await this.writeFile('.sqless/migrations/001_initial.sql', Handlebars.templates['001_initial.sql'], { entities });
         await this.writeFile('.sqless/migrations/001_initial_rollback.sql', Handlebars.templates['001_initial_rollback.sql'], { entities: entities.reverse() });
+
+        for (const [p, ops] of Object.entries(api.paths)) {
+            const entityMatch = p.match(pathPattern);
+            if (entityMatch && entityMatch[1]) {
+                const entity = entities.find(e => e.name === entityMatch[1]);
+                if (entity) {
+                    for (const op of Object.keys(ops)) {
+                        let delegate;
+                        switch (op.toLowerCase()) {
+                            case 'post':
+                                delegate = `.sqless/queries/add-${entity.name}.yaml`;
+                                await this.writeFile(delegate, Handlebars.templates['add-entity.yaml'], entity);
+                                break;
+                            case 'delete':
+                                delegate = `.sqless/queries/delete-${entity.name}.yaml`;
+                                await this.writeFile(delegate, Handlebars.templates['delete-entity.yaml'], entity);
+                                break;
+                            case 'put':
+                            case 'patch':
+                                delegate = `.sqless/queries/update-${entity.name}.yaml`;
+                                await this.writeFile(delegate, Handlebars.templates['update-entity.yaml'], entity);
+                                break;
+                        }
+                        entity.operations.push({ method: op.toLowerCase(), delegate });
+                    }
+                }
+            }
+        }
+
+        await this.writeFile('.sqless/sqless-config.yaml', Handlebars.templates['sqless-config.yaml'], { apiPath: this.config.apiPath.replace(/^\.[\/\\]/, ''), entities });
 
 
         return Promise.resolve();
